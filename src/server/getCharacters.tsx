23d02c4215bedc03937d2e55a33fadbffea3d675
Tadeo <domenechtadeo@outlook.es>
2022-02-10 15:42:34 +0100

    Create the redirect for deploy in netlify and create process.env

--- src/server/getCharacters.tsx
+++ src/server/getCharacters.tsx
@@ -1,5 +1,5 @@
 const getCharacters = async () => {
-  const url = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=2693b3abaa3a4e717ff114f9f0e0cf2a&hash=9c289834ae7ecae066cd236742048440`;
+  const url: any = process.env.REACT_APP_URL;
   const response = await fetch(url);
   const { data } = await response.json();
   const { results } = await data;
