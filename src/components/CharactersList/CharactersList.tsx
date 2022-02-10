24029831065d79f5f83987a76e6b49d2029b8bfa
Tadeo <domenechtadeo@outlook.es>
2022-02-10 13:15:58 +0100

    Create the styles for CharactersList component

--- src/components/CharactersList/CharactersList.tsx
+++ src/components/CharactersList/CharactersList.tsx
@@ -1,15 +1,18 @@
 import { ICharctersObject } from "../../interfaces/interfaces";
+import "./CharactersList.scss";
 
 const CharactersList = ({ character }: ICharctersObject) => {
   return (
     <div className="character">
       <h3 className="character__title">{character.name}</h3>
-      <p className="character__description">{character.description}</p>
-      <img
-        className="character__image"
-        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-        alt={character.name}
-      ></img>
+      <div className="character__info">
+        <img
+          className="character__image"
+          src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+          alt={character.name}
+        ></img>
+        <p className="character__description">{character.description}</p>
+      </div>
       <button className="character__button">More Info...</button>
     </div>
   );
