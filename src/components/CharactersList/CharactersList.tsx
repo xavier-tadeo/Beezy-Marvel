9eb52c716935a16b4be9b950aad658afe63c7be2
Tadeo <domenechtadeo@outlook.es>
2022-02-10 13:45:28 +0100

    Added style for desctiption character and condition for don t description

--- src/components/CharactersList/CharactersList.tsx
+++ src/components/CharactersList/CharactersList.tsx
@@ -11,7 +11,12 @@ const CharactersList = ({ character }: ICharctersObject) => {
           src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
           alt={character.name}
         ></img>
-        <p className="character__description">{character.description}</p>
+        <h5 className="character__title-description">Description</h5>
+        {!character.description ? (
+          <p className="character__description">Don't have a description</p>
+        ) : (
+          <p className="character__description">{character.description}</p>
+        )}
       </div>
       <button className="character__button">More Info...</button>
     </div>
