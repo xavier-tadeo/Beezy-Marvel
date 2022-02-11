57c0bd1a1594daabb06bc02e8e451170738080e3
Tadeo <domenechtadeo@outlook.es>
2022-02-11 12:53:24 +0100

    Added condicion for change order characters in server

--- src/server/getCharacters.tsx
+++ src/server/getCharacters.tsx
@@ -1,11 +1,16 @@
-const getCharacters = async () => {
-  const orderCharacters = "name";
+const getCharacters = async (orderCharacters: boolean) => {
+  let order;
+  if (orderCharacters) {
+    order = "name";
+  } else {
+    order = "-name";
+  }
   const offset = "0";
   const limit = "50";
 
   const url: any = process.env.REACT_APP_URL;
   const response = await fetch(
-    `${url}&orderBy=${orderCharacters}&offset=${offset}&limit=${limit}`
+    `${url}&orderBy=${order}&offset=${offset}&limit=${limit}`
   );
   const { data } = await response.json();
   const { results } = await data;
