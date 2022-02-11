b2ad44deace41b48dfcdfa135c531f4d5f5a0358
Tadeo <domenechtadeo@outlook.es>
2022-02-11 12:15:38 +0100

    Create new style for list of Characters

--- src/components/CharactersList/CharactersList.tsx
+++ src/components/CharactersList/CharactersList.tsx
@@ -6,21 +6,24 @@ import "./CharactersList.scss";
 const CharactersList = ({ character }: ICharactersObject) => {
   return (
     <div className="character">
-      <h3 className="character__title">{character.name}</h3>
       <div className="character__info">
-        <img
-          className="character__image"
-          src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-          alt={character.name}
-        ></img>
+        <h3 className="character__title">{character.name}</h3>
+
         <h5 className="character__title-description">Description</h5>
         {!character.description ? (
           <p className="character__description">Don't have a description</p>
         ) : (
           <p className="character__description">{character.description}</p>
         )}
+        <Link to={`/${character.id}`}>More Info...</Link>
       </div>
-      <Link to={`/${character.id}`}>More Info...</Link>
+      <div className="character__container-image">
+        <img
+          className="character__image"
+          src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+          alt={character.name}
+        ></img>
+      </div>
     </div>
   );
 };
