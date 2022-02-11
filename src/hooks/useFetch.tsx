db91d195caa015ecf3769bac93cbe1be8be6d02a
Tadeo <domenechtadeo@outlook.es>
2022-02-11 15:47:34 +0100

    Remove console.log to useFetch

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import getCharacters from "../server/getCharacters";
 
 const useFetch = (setCharactersArray: Function, orderCharacters: boolean) => {
-  console.log(orderCharacters);
   useEffect(() => {
     getCharacters(orderCharacters).then((results) => {
       setCharactersArray(results);
