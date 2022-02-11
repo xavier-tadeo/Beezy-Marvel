e78eaa7f97028bd5ffedb1a1409944210a55c103
Tadeo <domenechtadeo@outlook.es>
2022-02-11 13:10:03 +0100

    Added the function change order characters

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
+import { UserContext } from "../UserContext/UserContext";
 import "./Sort.scss";
 
 const Sort = () => {
+  const userContext = useContext(UserContext);
+
+  const { orderCharacters, setOrderCharacters }: any = userContext;
+
+  const handleOrder = () => {
+    setOrderCharacters(!orderCharacters);
+  };
+
   return (
     <div className="sort">
-      <button className="sort__button">Order Characters</button>
+      <button className="sort__button" onClick={handleOrder}>
+        Order Characters
+      </button>
     </div>
   );
 };
