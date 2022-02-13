161bf23b02122970e9a4c3dcd835cf4bbc8c7b47
Tadeo <domenechtadeo@outlook.es>
2022-02-13 18:38:00 +0100

    Create the fuction for pagination to the Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,41 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../UserContext/UserContext";
 import "./Footer.scss";
 
 const Footer = () => {
+  const userContext = useContext(UserContext);
+
+  const { offset, setOffset }: any = userContext;
+
+  const [isDisable, setIsDisable] = useState(true);
+
+  const handleNextiusPage = () => {
+    setOffset(offset + 20);
+
+    buttonDisable();
+  };
+
+  const handlePreviusPage = () => {
+    setOffset(offset - 20);
+
+    buttonDisable();
+  };
+  const buttonDisable = () => {
+    setIsDisable(false);
+  };
+
   return (
     <div className="footer">
-      <button className="footer__button">Previus</button>
-      <button className="footer__button">Next</button>
+      <button
+        className="footer__button"
+        onClick={handlePreviusPage}
+        disabled={isDisable}
+      >
+        Previus
+      </button>
+      <button className="footer__button" onClick={handleNextiusPage}>
+        Next
+      </button>
     </div>
   );
 };
