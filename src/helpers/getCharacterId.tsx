6ff44d63755c693421e828d32cee112aa2f4d564
Tadeo <domenechtadeo@outlook.es>
2022-02-15 11:10:11 +0100

    Delete return to helpers getCharacterId

--- src/helpers/getCharacterId.tsx
+++ src/helpers/getCharacterId.tsx
@@ -1,9 +1,8 @@
 import { ICharacters } from "../interfaces/interfaces";
 
-const getCharacterId = (id: any, charactersArray: any) => {
-  return charactersArray.find(
+const getCharacterId = (id: any, charactersArray: any) =>
+  charactersArray.find(
     (character: ICharacters) => JSON.stringify(character.id) === id
   );
-};
 
 export default getCharacterId;
