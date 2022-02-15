88dd837100417fed30392d541e6a8258d7651913
Tadeo <domenechtadeo@outlook.es>
2022-02-15 11:11:22 +0100

    Initial test for CharactersList

---
+++ src/components/CharactersList/CharactersList.test.tsx
@@ -0,0 +1,29 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+
+import { AppContext } from "../AppContext/AppContext";
+import CharactersList from "./CharactersList";
+
+describe("Given a CharacterList component", () => {
+  describe("When it render with a character info", () => {
+    describe("It should rendered a 'name' and 'image' of character", () => {
+      const character = {
+        name: "Hulk",
+        thumbnail: {
+          path: "https//imageHulk",
+          extension: ".jpg",
+        },
+        description: "History of hulk",
+        id: "1221",
+      };
+
+      render(
+        <AppContext>
+          <CharactersList character={character} />
+        </AppContext>
+      );
+
+      screen.getByText(character.name);
+    });
+  });
+});
