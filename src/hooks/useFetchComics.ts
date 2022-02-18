e1eebd174f3de127764a1c08e98d721c2ffce526
Tadeo <domenechtadeo@outlook.es>
2022-02-18 14:36:52 +0100

    Create the useFetchSeries

--- src/hooks/useFetchComics.ts
+++ src/hooks/useFetchComics.ts
@@ -4,7 +4,6 @@ import getCharactersComics from "../services/getCharactersComics";
 const useFetchComics = (id: any, setComicsArray: Function) => {
   useEffect(() => {
     getCharactersComics(id).then((data) => {
-      console.log(data);
       setComicsArray(data);
     });
   }, [id, setComicsArray]);
